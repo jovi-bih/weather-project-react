1f3285b423c32ed2a1973309bcd5e1df4e8d0e78
jovi-bih <99941708+jovi-bih@users.noreply.github.com>
2022-07-16 22:44:16 -0400

    Added files and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
 
-function App() {
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://github.com/jovi-bih/react-weather-app"
+            target="_blank"
+          >
+            Bih Jovi
+          </a>
+          {""}
+          and is {""}
+          <a
+            href="https://github.com/jovi-bih/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
